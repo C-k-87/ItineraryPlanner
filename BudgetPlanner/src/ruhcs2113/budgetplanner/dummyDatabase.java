554f6721371b906ed99bcd6c168536fa2d7e57ef
Charith Kularatne <108782491+C-k-87@users.noreply.github.com>
2023-05-14 16:54:58 +0530

    Add files via upload

---
+++ BudgetPlanner/src/ruhcs2113/budgetplanner/dummyDatabase.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ruhcs2113.budgetplanner;
+
+/**
+ *
+ * @author jack
+ */
+public class dummyDatabase {
+    static final double DUMMY1 = 100.00;
+    static final double DUMMY2 = 200.00;
+    static final double DUMMY3 = 300.00;
+    static final double DUMMY4 = 400.00;
+    static final double DUMMY5 = 500.00;
+    static final double DUMMY6 = 600.00;
+}
