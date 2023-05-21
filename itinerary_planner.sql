f8775aeb50395e9ccdcfb89a80d27c04db7f5eb8
Himantha-alt <124495416+Himantha-alt@users.noreply.github.com>
2023-05-21 11:03:09 +0530

    Final version

---
+++ itinerary_planner.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 21, 2023 at 03:55 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `itinerary_planner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photo`
+--
+
+CREATE TABLE `photo` (
+  `username` int(50) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `budget` float NOT NULL,
+  `category` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`task_id`, `uid`, `title`, `description`, `budget`, `category`) VALUES
+(1, 1, 'Test', 'test', 10, 'Attraction'),
+(1, 2, 'u2task1', 'desc', 100, 'Attraction'),
+(2, 1, 'task2', 'desc', 100, 'Entertaintment'),
+(2, 2, 'u2task2', 'desc', 300, 'Food'),
+(3, 1, 'task3', 'desc', 50, 'Food'),
+(3, 2, 'u2task3', 'desc', 700, 'Accommodation'),
+(4, 1, 'task4', 'desc', 200, 'Accommodation'),
+(4, 2, 'u2task4', 'desc', 1500, 'Transport'),
+(5, 1, 'task5', 'desc', 400, 'Transport');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tourist`
+--
+
+CREATE TABLE `tourist` (
+  `uid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `dob` varchar(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `tpno` char(10) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `uname` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tourist`
+--
+
+INSERT INTO `tourist` (`uid`, `name`, `dob`, `email`, `address`, `tpno`, `gender`, `uname`, `password`) VALUES
+(1, 'Charith', '2000', 'cbkule@gmail.com', 'Matale', '0703004073', 'm', 'cbkule', '1234'),
+(2, 'User', '1999', 'abcd', 'Matara', '070', 'm', 'user', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`,`uid`);
+
+--
+-- Indexes for table `tourist`
+--
+ALTER TABLE `tourist`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tourist`
+--
+ALTER TABLE `tourist`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
